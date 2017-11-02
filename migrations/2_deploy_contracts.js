05cdb87fcb42722c5f91fd46637a2c2f59650012
phil.elsasser <phil@marketprotocol.io>
2017-11-02 07:49:39 -0600

    adding example deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var MarketContract = artifacts.require("./MarketContract.sol");
+var MathLib = artifacts.require("./libraries/MathLib.sol");
 
+// example of deploying a contract using kraken and ETH/BTC and repeating queries every 30 seconds
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(MathLib);
+  deployer.link(MathLib, MarketContract);
+  deployer.deploy(MarketContract, "ETHXBT",
+  "0x0", "URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0", 30,
+   20155, 60465,  6, 60 * 15, {gas:5000000 ,  value: web3.toWei('.2', 'ether'), from: web3.eth.accounts[0]});
 };
