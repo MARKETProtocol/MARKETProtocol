0f68d12d541cf67a604e51bde818daa47f27f9dc
phil.elsasser <phil@marketprotocol.io>
2017-11-09 08:44:54 -0700

    updated truffle

--- truffle.js
+++ truffle.js
@@ -1,9 +1,12 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
     }
   }
-};
+}
