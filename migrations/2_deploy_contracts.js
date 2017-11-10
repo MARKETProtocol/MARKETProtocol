f3aac03ce314e9cb070f88b49bddc670d9cbef99
phil.elsasser <phil@marketprotocol.io>
2017-11-10 06:52:47 -0700

    updated deploy to include new libs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,9 @@
 var MarketContract = artifacts.require("./MarketContract.sol");
 var MathLib = artifacts.require("./libraries/MathLib.sol");
 var HashLib = artifacts.require("./libraries/HashLib.sol");
+var ZeppelinERC20 = artifacts.require("zeppelin-solidity/contracts/token/ERC20.sol");
+var ZeppelinSafeERC20 = artifacts.require("zeppelin-solidity/contracts/token/SafeERC20.sol");
+
 
 // example of deploying a contract using kraken and ETH/BTC and repeating queries every 30 seconds
 module.exports = function(deployer) {
@@ -8,6 +11,10 @@ module.exports = function(deployer) {
   deployer.link(MathLib, MarketContract);
   deployer.deploy(HashLib);
   deployer.link(HashLib, MarketContract);
+  deployer.deploy(ZeppelinERC20);
+  deployer.link(ZeppelinERC20, MarketContract);
+  deployer.deploy(ZeppelinSafeERC20);
+  deployer.link(ZeppelinSafeERC20, MarketContract);
   deployer.deploy(MarketContract, "ETHXBT",
   "0x0", "URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0", 30,
    20155, 60465,  6, 60 * 15, {gas:5000000 ,  value: web3.toWei('.2', 'ether'), from: web3.eth.accounts[0]});
