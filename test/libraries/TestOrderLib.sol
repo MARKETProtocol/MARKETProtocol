3f6ba980148c7bdbe971d9591f88e88ef7ae6a4c
Phil Elsasser <phil@marketprotocol.io>
2017-11-16 09:32:27 -0700

    adding addresses to test hashing

--- test/libraries/TestOrderLib.sol
+++ test/libraries/TestOrderLib.sol
@@ -22,5 +22,8 @@ import "../../contracts/libraries/OrderLib.sol";
 /// @title TestOrderLib tests for all of our order helper functions
 /// @author Phil Elsasser <phil@marketprotcol.io>
 contract TestOrderLib {
-
+    address private MAKER = 0x627306090abaB3A6e1400e9345bC60c78a8BEf57;
+    address private TAKER = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
+    address private FEE_RECIPIENT = 0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef;
+    address private OUTSIDE_ADDRESS = 0x2191eF87E392377ec08E7c08Eb105Ef5448eCED5;  //used as check
 }
