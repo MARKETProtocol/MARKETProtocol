c9c016583dff5df38b0d8185da750830d4447189
Phil Elsasser <phil@marketprotocol.io>
2017-11-16 15:44:49 -0700

    commenting out network addresses

--- truffle.js
+++ truffle.js
@@ -1,9 +1,9 @@
 module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // Match any network id
-    }
-  }
+//  networks: {
+//    development: {
+//      host: 'localhost',
+//      port: 8545,
+//      network_id: '*' // Match any network id
+//    }
+//  }
 }
