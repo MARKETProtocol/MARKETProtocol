2699833b3c24d688db198fad9cef1d67a48df7f0
Phil Elsasser <phil@marketprotocol.io>
2017-11-16 16:28:12 -0700

    adding dummy token

---
+++ contracts/tokens/CollateralToken.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.4.18;
+
+import "zeppelin-solidity/contracts/examples/SimpleToken.sol";
+
+// dummy ERC20 token for testing purposes
+contract CollateralToken is SimpleToken {
+
+}
