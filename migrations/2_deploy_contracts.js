f2d7fbcf22117900c9793b95dc31b84c070456f2
Phil Elsasser <phil@marketprotocol.io>
2017-11-16 16:28:29 -0700

    updating deploy with dummy token to use for collateral

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,11 @@ var MarketContract = artifacts.require("./MarketContract.sol");
 var MathLib = artifacts.require("./libraries/MathLib.sol");
 var OrderLib = artifacts.require("./libraries/OrderLib.sol");
 var ZeppelinSafeERC20 = artifacts.require("zeppelin-solidity/contracts/token/SafeERC20.sol");
+var CollateralToken = artifacts.require("./tokens/CollateralToken.sol");
 
 
-// example of deploying a contract using kraken and ETH/BTC and repeating queries every 30 seconds
+// example of deploying a contract using a basic ERC20 token example as collateral
+// and uses kraken's ETH/BTC price repeating  the queries every 30 seconds
 module.exports = function(deployer) {
   deployer.deploy(MathLib);
   deployer.link(MathLib, MarketContract);
@@ -12,7 +14,19 @@ module.exports = function(deployer) {
   deployer.link(OrderLib, MarketContract);
   deployer.deploy(ZeppelinSafeERC20);
   deployer.link(ZeppelinSafeERC20, MarketContract);
-  deployer.deploy(MarketContract, "ETHXBT",
-  "0x0", "URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0", 30,
-   20155, 60465,  6, 1000, 60 * 15, {gas:6000000 ,  value: web3.toWei('.2', 'ether'), from: web3.eth.accounts[0]});
+  deployer.deploy(CollateralToken).then(function() {
+    return deployer.deploy(
+                        MarketContract,
+                        "ETHXBT",
+                        CollateralToken.address,
+                        "URL",
+                        "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0",
+                        30,
+                        20155,
+                        60465,
+                        6,
+                        1000,
+                        60 * 15,
+                        {gas:6000000 ,  value: web3.toWei('.2', 'ether'), from: web3.eth.accounts[0]})
+  })
 };
