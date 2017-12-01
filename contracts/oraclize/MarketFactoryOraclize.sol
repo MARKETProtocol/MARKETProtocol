ae691cb945963f69337cf85a3ec385eac9219522
Phil Elsasser <phil@marketprotocol.io>
2017-12-01 11:56:45 -0700

    brought up to date...still doesn't work with gas contstraints.

--- contracts/oraclize/MarketFactoryOraclize.sol
+++ contracts/oraclize/MarketFactoryOraclize.sol
@@ -16,7 +16,7 @@
 
 pragma solidity 0.4.18;
 
-//import "./MarketContract.sol";
+import "./MarketContractOraclize.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
@@ -24,70 +24,68 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 /// deployed.  Currently this idea is pretty limited by deployment gas, so leave this as place holder for future
 /// ideas.
 /// @author Phil Elsasser <phil@marketprotcol.io>
-contract MarketFactory is Ownable {
+contract MarketFactoryOraclize is Ownable {
+
+    address public MKT_TOKEN_ADDRESS;
 
     mapping(address => bool) isWhiteListed; // by default currently all contracts are whitelisted
     address[] deployedAddresses;            // record of all deployed addresses;
 
-    event MarketContractDeployed(address);
+    event MarketContractDeployed(address indexed marketContractAddress);
 
-    function MarketFactory() public {
-
+    function MarketFactoryOraclize(address marketTokenAddress) public {
+        MKT_TOKEN_ADDRESS = marketTokenAddress;
     }
 
     /// @notice deploys a market contract and adds the new address to the white list to allow trading
     /// @param contractName viewable name of this contract (BTC/ETH, LTC/ETH, etc)
     /// @param baseTokenAddress address of the ERC20 token that will be used for collateral and pricing
+    /// @param contractSpecs array of unsigned integers including:
+    /// floorPrice minimum tradeable price of this contract, contract enters settlement if breached
+    /// capPrice maximum tradeable price of this contract, contract enters settlement if breached
+    /// priceDecimalPlaces number of decimal places to convert our queried price from a floating point to
+    /// an integer
+    /// qtyDecimalPlaces decimal places to multiply traded qty by.
+    /// expirationTimeStamp - seconds from epoch that this contract expires and enters settlement
     /// @param oracleDataSource a data-source such as "URL", "WolframAlpha", "IPFS"
     /// see http://docs.oraclize.it/#ethereum-quick-start-simple-query
     /// @param oracleQuery see http://docs.oraclize.it/#ethereum-quick-start-simple-query for examples
     /// @param oracleQueryRepeatSeconds how often to repeat this callback to check for settlement, more frequent
     /// queries require more gas and may not be needed.
-    /// @param floorPrice minimum tradeable price of this contract, contract enters settlement if breached
-    /// @param capPrice maximum tradeable price of this contract, contract enters settlement if breached
-    /// @param priceDecimalPlaces number of decimal places to convert our queried price from a floating point to
-    /// an integer
-    /// @param qtyDecimalPlaces decimal places to multiply traded qty by.
-    /// @param secondsToExpiration - second from now that this contract expires and enters settlement
-    function deployMarketContract (
+    function deployMarketContractOraclize (
         string contractName,
         address baseTokenAddress,
+        uint[5] contractSpecs,
         string oracleDataSource,
         string oracleQuery,
-        uint oracleQueryRepeatSeconds,
-        uint floorPrice,
-        uint capPrice,
-        uint priceDecimalPlaces,
-        uint qtyDecimalPlaces,
-        uint secondsToExpiration
+        uint oracleQueryRepeatSeconds
     ) external payable returns (address)
     {
-//        // create market contract, forwarding amount payed to the constructor
-//        // not that msg.sender in market contract will now be our address, so we need to
-//        // ammend constructor to accept the address of the caller of THIS function as the creator
-//        // of the contract.
-//        MarketContract marketContract = (new MarketContract).value(msg.value)(
-//             contractName,
-//             baseTokenAddress,
-//             oracleDataSource,
-//             oracleQuery,
-//             oracleQueryRepeatSeconds,
-//             floorPrice,
-//             capPrice,
-//             priceDecimalPlaces,
-//             qtyDecimalPlaces,
-//             secondsToExpiration
-//        );
-//        isWhiteListed[address(marketContract)] = true;
-//        deployedAddresses.push(address(marketContract));
-//        MarketContractDeployed(address(marketContract));
-//        return address(marketContract);
+        // create market contract, forwarding amount payed to the constructor
+        // not that msg.sender in market contract will now be our address, so we need to
+        // ammend constructor to accept the address of the caller of THIS function as the creator
+        // of the contract.
+        MarketContractOraclize marketContract = (new MarketContractOraclize).value(msg.value)(
+            contractName,
+            MKT_TOKEN_ADDRESS,
+            baseTokenAddress,
+            contractSpecs,
+            oracleDataSource,
+            oracleQuery,
+            oracleQueryRepeatSeconds
+        );
+
+        isWhiteListed[address(marketContract)] = true;
+        deployedAddresses.push(address(marketContract));
+        MarketContractDeployed(address(marketContract));
+        return address(marketContract);
     }
 
     /// @notice determines if an address is a valid MarketContract
     /// @return false if the address has not been deployed by this factory, or is no longer white listed.
-    function isAddressWhiteListed(address contractAddress) external view returns (bool) {
-        return isWhiteListed[contractAddress];
+    function isAddressWhiteListed(address contractAddress) public view returns (bool) {
+        return isWhiteListed[contractAddress] &&
+            MarketContractOraclize(contractAddress).isCollateralPoolContractLinked();
     }
 
     /// @notice the current number of contracts that have been deployed by this factory.
@@ -100,7 +98,7 @@ contract MarketFactory is Ownable {
     /// @return address of a white listed contract, or if contract is no longer valid address(0) is returned.
     function getAddressByIndex(uint index) external view returns (address) {
         address deployedAddress = deployedAddresses[index];
-        if (isWhiteListed[deployedAddress]) {
+        if (isAddressWhiteListed(deployedAddress)) {
             return deployedAddress;
         } else {
             return address(0);
@@ -113,5 +111,4 @@ contract MarketFactory is Ownable {
     function removeContractFromWhiteList(address contractAddress) external onlyOwner returns (bool) {
         isWhiteListed[contractAddress] = false;
     }
-
 }
