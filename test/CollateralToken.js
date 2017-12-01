67c8c116a984b3546ab75a8c273ede0cba47810b
Phil Elsasser <phil@marketprotocol.io>
2017-12-01 13:12:28 -0700

    adding some basic tests.

---
+++ test/CollateralToken.js
@@ -0,0 +1,37 @@
+const CollateralToken = artifacts.require("CollateralToken");
+
+var initBalance;
+var collateralToken;
+var balancePerAcct;
+
+// basic tests to ensure collateral token works and is set up to allow trading
+contract('CollateralToken', function(accounts) {
+
+    // test setup of token for collateral
+    it("Initial supply should be 1e+22", async function() {
+        collateralToken = await CollateralToken.deployed();
+        initBalance = await collateralToken.INITIAL_SUPPLY.call().valueOf();
+        assert.equal(initBalance, 1e+22, "Initial balance not as expected");
+    });
+
+    it("Main account should have entire balance", async function() {
+        let mainAcctBalance = await collateralToken.balanceOf.call(accounts[0]).valueOf();
+        assert.equal(
+            mainAcctBalance.toNumber(),
+            initBalance.toNumber(),
+            "Entire coin balance should be in primary account"
+        );
+    });
+
+    it("Main account should be able to transfer balance", async function() {
+        var balanceToTransfer = initBalance / 2;
+        balancePerAcct = balanceToTransfer;
+        await collateralToken.transfer(accounts[1], balanceToTransfer, {from: accounts[0]});
+        let secondAcctBalance = await collateralToken.balanceOf.call(accounts[1]).valueOf();
+        assert.equal(
+            secondAcctBalance.toNumber(),
+            balanceToTransfer,
+            "Transfer didn't register correctly"
+        );
+    });
+});
---
+++ test/MarketCollateralPool.js
@@ -0,0 +1,78 @@
+const MarketContractOraclize = artifacts.require("MarketContractOraclize");
+const MarketCollateralPool = artifacts.require("MarketCollateralPool");
+const CollateralToken = artifacts.require("CollateralToken");
+const MarketToken = artifacts.require("MarketToken");
+
+
+// basic tests to ensure MarketCollateralPool works and is set up to allow trading
+contract('MarketCollateralPool', function(accounts) {
+
+    var balancePerAcct;
+    var collateralToken;
+    var initBalance;
+    var collateralPool;
+    var marketContract;
+    var marketToken
+
+    it("Both accounts should be able to deposit to collateral pool contract", async function() {
+        collateralToken = await CollateralToken.deployed();
+        initBalance = await collateralToken.INITIAL_SUPPLY.call().valueOf();
+        collateralPool = await MarketCollateralPool.deployed();
+        marketContract = await MarketContractOraclize.deployed();
+        marketToken = await MarketToken.deployed();
+
+        // transfer half of balance to second account
+        var balanceToTransfer = initBalance / 2;
+        await collateralToken.transfer(accounts[1], balanceToTransfer, {from: accounts[0]});
+
+        // currently the Market Token is deployed with no qty need to trade, so all accounts should
+        // be enabled.
+        let isAllowedToTradeAcctOne = await marketToken.isUserEnabledForContract(marketContract.address, accounts[0])
+        let isAllowedToTradeAcctTwo = await marketToken.isUserEnabledForContract(marketContract.address, accounts[1])
+
+        assert.isTrue(isAllowedToTradeAcctOne, "account isn't able to trade!");
+        assert.isTrue(isAllowedToTradeAcctTwo, "account isn't able to trade!");
+
+        var amountToDeposit = 5000000;
+        // create approval for main contract to move tokens!
+        await collateralToken.approve(collateralPool.address, amountToDeposit, {from: accounts[0]})
+        await collateralToken.approve(collateralPool.address, amountToDeposit, {from: accounts[1]})
+
+        // move tokens to the collateralPool
+        await collateralPool.depositTokensForTrading(amountToDeposit, {from: accounts[0]})
+        await collateralPool.depositTokensForTrading(amountToDeposit, {from: accounts[1]})
+
+        // ensure balances are now inside the contract.
+        let tradingBalanceAcctOne = await collateralPool.getUserAccountBalance.call(accounts[0]);
+        let tradingBalanceAcctTwo = await collateralPool.getUserAccountBalance.call(accounts[1]);
+        assert.equal(tradingBalanceAcctOne, amountToDeposit, "Balance doesn't equal tokens deposited");
+        assert.equal(tradingBalanceAcctTwo, amountToDeposit, "Balance doesn't equal tokens deposited");
+    });
+
+    it("Both accounts should be able to withdraw from collateral pool contract", async function() {
+        var amountToWithdraw = 2500000;
+        // move tokens to the MarketContract
+        await collateralPool.withdrawTokens(amountToWithdraw, {from: accounts[0]})
+        await collateralPool.withdrawTokens(amountToWithdraw, {from: accounts[1]})
+        // ensure balances are now correct inside the contract.
+        let tradingBalanceAcctOne = await collateralPool.getUserAccountBalance.call(accounts[0]);
+        let tradingBalanceAcctTwo = await collateralPool.getUserAccountBalance.call(accounts[1]);
+
+        assert.equal(
+            tradingBalanceAcctOne,
+            amountToWithdraw,
+            "Balance doesn't equal tokens deposited - withdraw"
+        );
+        assert.equal(
+            tradingBalanceAcctTwo,
+            amountToWithdraw,
+            "Balance doesn't equal tokens deposited - withdraw"
+        );
+
+        // ensure balances are now correct inside the arbitrary token
+        balancePerAcct = initBalance / 2;
+        var expectedTokenBalances = balancePerAcct - tradingBalanceAcctOne;
+        let secondAcctTokenBalance = await collateralToken.balanceOf.call(accounts[1]).valueOf();
+        assert.equal(secondAcctTokenBalance, expectedTokenBalances, "Token didn't get transferred back to user");
+    });
+});
