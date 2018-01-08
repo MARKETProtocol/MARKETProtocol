0554f0bfff5c14f38617e64cb7ef3de6b49a6e42
Cryptomental <cryptomental.com@gmail.com>
2018-01-08 13:03:08 +0100

    scripts/coverage_run.sh: Run coveralls in verbose mode.
    Run coveralls in verbose mode.
    
    Issue: #21

--- scripts/coverage_run.sh
+++ scripts/coverage_run.sh
@@ -3,5 +3,5 @@
 node_modules/.bin/solidity-coverage
 
 if [ "$CONTINUOUS_INTEGRATION" = true ]; then
-  cat coverage/lcov.info | node_modules/.bin/coveralls
+  cat coverage/lcov.info | node_modules/.bin/coveralls --verbose
 fi
