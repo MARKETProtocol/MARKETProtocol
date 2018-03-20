f9dc48dd4ca526da790b43b8945e74f1bd67a769
Cryptomental <cryptomental.com@gmail.com>
2018-03-20 09:40:03 +0100

    test/GasOraclize: Update test case for Oraclize callback.
    Update the test. Verify that also UpdatedLastPrice event was triggered.
    
    Issue: #57

--- test/GasOraclize.js
+++ test/GasOraclize.js
@@ -35,6 +35,7 @@ contract('MarketContractOraclize.CallBackExpiration', function(accounts) {
             gasLimit = block.gasLimit;
         }
         let txReceipt = null;  // Oraclize callback transaction receipt
+
         MarketContractOraclize.new(
             "ETHUSD-10",
             marketToken.address,
@@ -44,39 +45,47 @@ contract('MarketContractOraclize.CallBackExpiration', function(accounts) {
             "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0",
             { gas: gasLimit, value: web3.toWei('.1', 'ether'), accountMaker}
         ).then(async function(deployedMarketContract) {
-
             let oraclizeCallbackGasCost = await deployedMarketContract.QUERY_CALLBACK_GAS.call();
             let contractSettledEvent = deployedMarketContract.ContractSettled();
+            let updatedLastPriceEvent = deployedMarketContract.UpdatedLastPrice();
+
             contractSettledEvent.watch(async (err, response) => {
                 if (err) {
                     console.log(err);
                 };
-
                 assert.equal(response.event, 'ContractSettled');
                 txReceipt = await web3.eth.getTransactionReceipt(response.transactionHash);
                 console.log("Oraclize callback gas used : " + txReceipt.gasUsed);
                 assert.isBelow(txReceipt.gasUsed, oraclizeCallbackGasCost,
                                "Callback tx claims to have used more gas than allowed!");
                 contractSettledEvent.stopWatching();
-                return response;
+            });
+
+            updatedLastPriceEvent.watch(async (err, response) => {
+                if (err) {
+                    console.log(err);
+                };
+                assert.equal(response.event, 'UpdatedLastPrice');
+                await web3.eth.getTransactionReceipt(response.transactionHash);
+                updatedLastPriceEvent.stopWatching();
             });
         });
 
-        let waitForContractSettledEvent = ms => new Promise((r, j) => {
-            var check = () => {
-              if (txReceipt)
-                r()
-              else if((ms -= 1000) < 0)
-                j('Oraclize time out!')
-              else
-                setTimeout(check, 1000)
-            }
-            setTimeout(check, 1000)
-        });
+    let waitForContractSettledEvent = ms => new Promise((r, j) => {
+        var check = () => {
+          if (txReceipt)
+            r()
+          else if((ms -= 1000) < 0)
+            j('Oraclize time out!')
+          else {
+            setTimeout(check, 1000);
+          }
+        }
+        setTimeout(check, 1000)
+    });
 
-        await waitForContractSettledEvent(30000);
-        console.log(txReceipt);
-        assert.notEqual(txReceipt, null, "Oraclize callback did not arrive. Please increase QUERY_CALLBACK_GAS!");
+    await waitForContractSettledEvent(30000);
+    assert.notEqual(txReceipt, null, "Oraclize callback did not arrive. Please increase QUERY_CALLBACK_GAS!");
 
     })
 });
