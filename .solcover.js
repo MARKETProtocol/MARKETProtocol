a4882b40f62c03d9ddfa3d5f9bf84ff870609c7d
Cryptomental <cryptomental.com@gmail.com>
2018-05-15 13:16:59 +0200

    .solcover.js: Update path to openzeppelin-solidity.
    The path must be corrected to copy the package into coverage environment.
    
    Issue: #73

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,6 @@ module.exports = {
     skipFiles: ['oraclize/oraclizeAPI.sol', 'Migrations.sol', 'oraclize/OraclizeQueryTest.sol' ],
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    copyPackages: ['zeppelin-solidity'],
+    copyPackages: ['openzeppelin-solidity'],
     norpc: true
 };
