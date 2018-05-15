a52f984d5fce407c9d365fd4c2156207f6f703b5
Cryptomental <cryptomental.com@gmail.com>
2018-05-15 14:28:12 +0200

    oraclizeAPI.sol: Remove unused query2 and queryN functions.
    Issue: #73

--- contracts/oraclize/oraclizeAPI.sol
+++ contracts/oraclize/oraclizeAPI.sol
@@ -34,10 +34,6 @@ contract OraclizeI {
     address public cbAddress;
     function query(uint _timestamp, string _datasource, string _arg) external payable returns (bytes32 _id);
     function query_withGasLimit(uint _timestamp, string _datasource, string _arg, uint _gaslimit) external payable returns (bytes32 _id);
-    function query2(uint _timestamp, string _datasource, string _arg1, string _arg2) public payable returns (bytes32 _id);
-    function query2_withGasLimit(uint _timestamp, string _datasource, string _arg1, string _arg2, uint _gaslimit) external payable returns (bytes32 _id);
-    function queryN(uint _timestamp, string _datasource, bytes _argN) public payable returns (bytes32 _id);
-    function queryN_withGasLimit(uint _timestamp, string _datasource, bytes _argN, uint _gaslimit) external payable returns (bytes32 _id);
     function getPrice(string _datasource) public returns (uint _dsprice);
     function getPrice(string _datasource, uint gaslimit) public returns (uint _dsprice);
     function setProofType(byte _proofType) external;
