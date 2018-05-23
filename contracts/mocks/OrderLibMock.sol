3fcb01be781eaee4ca31dcf5ba2aa71107760c51
Phil Elsasser <phil@marketprotocol.io>
2018-05-23 15:02:45 -0600

    add license to mock

--- contracts/mocks/OrderLibMock.sol
+++ contracts/mocks/OrderLibMock.sol
@@ -1,13 +1,28 @@
+/*
+    Copyright 2017-2018 Phillip A. Elsasser
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 pragma solidity ^0.4.23;
 
 import "../libraries/OrderLib.sol";
 
 /// @title OrderLibMock
 /// Mock contract for library OrderLib.sol
-/// OrderLibMock allows for direct calling of non-view library methods.
-/// functions are preceded by _ to indicate that they are a mock function and not
+/// OrderLibMock allows for direct calling of non-view library methods in solidity-coverage.
+/// Functions are preceded by _ to indicate that they are a mock function and not
 /// a function re-declaration.
-
 contract OrderLibMock {
   using OrderLib for OrderLib.OrderMappings;
   using OrderLib for OrderLib.Order;
