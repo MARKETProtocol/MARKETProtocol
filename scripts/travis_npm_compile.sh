5d085314890d96cca6278fe2d9fb9279991bcade
Phil Elsasser <phil@marketprotocol.io>
2018-06-06 14:55:38 -0600

    fix travis script to deal with TRAVIS_BRANCH on tags

--- scripts/travis_npm_compile.sh
+++ scripts/travis_npm_compile.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # if we are on master and it has a tag, prepare build artifacts for npm package
-if [ "$TRAVIS_BRANCH" = "master" ] && [[ -v TRAVIS_TAG ]]; then
+if [[ -v TRAVIS_TAG ]]
+then
   npm install
   npm install -g truffle
   truffle compile --all
