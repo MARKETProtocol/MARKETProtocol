cabaa6d50b1423f739e39eeabc76746904dea4fe
Phil Elsasser <phil@marketprotocol.io>
2018-06-25 11:33:55 -0600

    add new InitialAllocationCollateralToken for our sim

---
+++ test/tokens/InitialAllocationCollateralToken.js
@@ -0,0 +1,46 @@
+const CollateralToken = artifacts.require('InitialAllocationCollateralToken');
+
+// basic tests to ensure collateral token allows for initial grant.
+contract('InitialAllocationCollateralToken', function(accounts) {
+
+  let collateralToken;
+
+  before(async function() {
+    return CollateralToken.new('CollateralToken', 'CTK', 100, 18, {
+      gas: 2000000,
+      from: web3.eth.accounts[0]
+    }).then(function(instance) {
+      collateralToken = instance;
+      return collateralToken;
+    });
+  });
+
+  it('account should not have balance', async function() {
+    const subAcctBalance = await collateralToken.balanceOf.call(accounts[1]).valueOf();
+    assert.equal(
+      subAcctBalance.toNumber(),
+      0,
+      'balances should be zero for all accounts that did not deploy the token'
+    );
+  });
+
+  it('account should be able to unlock balance only once', async function() {
+    await collateralToken.getInitialAllocation({from: web3.eth.accounts[1]});
+    const subAcctBalance = await collateralToken.balanceOf.call(accounts[1]).valueOf();
+    const initialAllocation = await collateralToken.INITIAL_TOKEN_ALLOCATION.call().valueOf();
+    assert.equal(
+      subAcctBalance.toNumber(),
+      initialAllocation.toNumber(),
+      'balances should be allocated to called of getInitialAllocation'
+    );
+
+    let error = null;
+    try {
+      await collateralToken.getInitialAllocation({from: web3.eth.accounts[1]});
+    } catch (err) {
+      error = err;
+    }
+    assert.ok(error instanceof Error, 'Caller was able to get initial allocation twice!');
+
+  });
+});
