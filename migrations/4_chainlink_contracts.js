6ee5dbfb47f61e237274f465a3c6eb4aaf92c3f8
Phil Elsasser <phil@marketprotocol.io>
2018-09-10 15:44:01 -0600

    add start of new migrations

--- migrations/4_chainlink_contracts.js
+++ migrations/4_chainlink_contracts.js
@@ -1,2 +1,11 @@
-module.exports = function(deployer, network) {
+const LinkToken = artifacts.require('./chainlink/src/lib/LinkToken.sol');
+const ChainLinkOracle = artifacts.require('./chainlink/src/Oracle.sol');
+
+
+module.exports = function(deployer) {
+  deployer.deploy(LinkToken).then( function() {
+    deployer.deploy(ChainLinkOracle, LinkToken.address).then( function() {
+      //deployer.deploy(MyContract, LinkToken.address, ChainLinkOracle.address);
+    });
+  });
 };
