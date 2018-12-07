73b2d5bc566e0a44c9d6bdc23462509a63d17391
Perfect Makanju <damiperfect@gmail.com>
2018-12-07 14:39:02 +0100

    Link MathLib with MMarketContractFactoryChainLink

--- migrations/4_chainlink_contracts.js
+++ migrations/4_chainlink_contracts.js
@@ -1,3 +1,4 @@
+const MathLib = artifacts.require('./libraries/MathLib.sol');
 const LinkToken = artifacts.require('./chainlink/src/lib/LinkToken.sol');
 const ChainLinkOracle = artifacts.require('./chainlink/src/Oracle.sol');
 const MarketContractFactory = artifacts.require('./chainlink/MarketContractFactoryChainLink.sol');
@@ -15,37 +16,39 @@ module.exports = function (deployer, network) {
     return deployer.deploy(LinkToken).then(function (linkToken) {
       return deployer.deploy(ChainLinkOracle, LinkToken.address).then(function () {
         return MarketContractRegistry.deployed().then(function (registry) {
-          return deployer.deploy(
-            MarketContractFactory,
-            registry.address,
-            MarketToken.address,
-            {gas: gasLimit, from: web3.eth.accounts[0]}
-          ).then(function (factory) {
-            return registry.addFactoryAddress(factory.address).then(function () {
-              return deployer.deploy(
-                OracleHub,
-                factory.address,
-                LinkToken.address,
-                ChainLinkOracle.address,
-                {gas: gasLimit, from: web3.eth.accounts[0]}
-              ).then(function (oracleHub) {
-                // set the oracle hub address in the factory to allow to request queries.
-                return factory.setOracleHubAddress(
-                  oracleHub.address,
-                  {from: web3.eth.accounts[0]}
-                ).then(function () {
-                  // transfer link token to hub to pay for queries.
-                  return linkToken.transfer(oracleHub.address, 10e22).then(function () {
-                    return factory.deployMarketContractChainLink(
-                      'ETHXBT',
-                      CollateralToken.address,
-                      [20155, 60465, 2, 10, marketContractExpiration],
-                      'https://api.kraken.com/0/public/Ticker?pair=ETHUSD',
-                      'result.XETHZUSD.c.0',
-                      'fakeSleepJobId',
-                      'fakeOnDemandJobId',
-                      {gas: gasLimit, from: web3.eth.accounts[0]}
-                    )
+          deployer.link(MathLib, [MarketContractFactory]).then(function () {
+            return deployer.deploy(
+              MarketContractFactory,
+              registry.address,
+              MarketToken.address,
+              {gas: gasLimit, from: web3.eth.accounts[0]}
+            ).then(function (factory) {
+              return registry.addFactoryAddress(factory.address).then(function () {
+                return deployer.deploy(
+                  OracleHub,
+                  factory.address,
+                  LinkToken.address,
+                  ChainLinkOracle.address,
+                  {gas: gasLimit, from: web3.eth.accounts[0]}
+                ).then(function (oracleHub) {
+                  // set the oracle hub address in the factory to allow to request queries.
+                  return factory.setOracleHubAddress(
+                    oracleHub.address,
+                    {from: web3.eth.accounts[0]}
+                  ).then(function () {
+                    // transfer link token to hub to pay for queries.
+                    return linkToken.transfer(oracleHub.address, 10e22).then(function () {
+                      return factory.deployMarketContractChainLink(
+                        'ETHXBT',
+                        CollateralToken.address,
+                        [20155, 60465, 2, 10, marketContractExpiration],
+                        'https://api.kraken.com/0/public/Ticker?pair=ETHUSD',
+                        'result.XETHZUSD.c.0',
+                        'fakeSleepJobId',
+                        'fakeOnDemandJobId',
+                        {gas: gasLimit, from: web3.eth.accounts[0]}
+                      );
+                    });
                   });
                 });
               });
