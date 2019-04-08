45efab79d16a2e3cefb76b807f76b24e8edcabeb
Phil Elsasser <phil@marketprotocol.io>
2019-04-08 15:01:08 -0600

    remove docker, updat travis.yml

--- scripts/coverage_run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-node_modules/.bin/solidity-coverage
-
-if [ "$CONTINUOUS_INTEGRATION" = true ]; then
-  cat coverage/lcov.info | node_modules/.bin/coveralls --verbose
-fi
--- scripts/travis_npm_compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# if we are on master and it has a tag, prepare build artifacts for npm package
-if [[ -v TRAVIS_TAG ]]
-then
-  npm install
-  npm install -g truffle
-  truffle compile --all
-fi
