f19e8f668b6e3e50135b2618ec057c921fd35170
Phil Elsasser <phil@marketprotocol.io>
2019-04-08 18:04:17 -0600

    update test rpc commands

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
-    skipFiles: ['oraclize/oraclizeAPI.sol', 'Migrations.sol', 'oraclize/OraclizeQueryTest.sol', 'mocks/OrderLibMock.sol' ],
+    skipFiles: ['Migrations.sol', 'mocks/OrderLibMock.sol', 'tokens/InitialAllocationCollateralToken.sol', 'tokens/CollateralToken.sol', 'tokens/UpgradeableTokenMock.sol' ],
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
     copyPackages: ['openzeppelin-solidity'],
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:9.2.0
 
-RUN npm install -g truffle@4.1.15 ethereumjs-testrpc-sc@6.1.2
+RUN npm install -g truffle@4.1.15 ganache-cli@6.4.2
 
 WORKDIR /app
 
