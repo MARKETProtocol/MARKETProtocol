7e89c37e76920515624775684f44eeb17bd4eed0
Phil Elsasser <phil@marketprotocol.io>
2019-05-17 20:32:10 -0600

    CollateralToken.js test working

--- test/tokens/CollateralToken.js
+++ test/tokens/CollateralToken.js
@@ -1,3 +1,4 @@
+const BN = require('bn.js');
 const CollateralToken = artifacts.require('CollateralToken');
 
 // basic tests to ensure collateral token works and is set up to allow trading
@@ -7,7 +8,7 @@ contract('CollateralToken', function(accounts) {
   // test setup of token for collateral
   it('Initial supply should be 1e+22', async function() {
     collateralToken = await CollateralToken.deployed();
-    initBalance = await collateralToken.totalSupply();
+    initBalance = await collateralToken.totalSupply.call();
     assert.equal(initBalance, 1e22, 'Initial balance not as expected');
   });
 
@@ -20,8 +21,10 @@ contract('CollateralToken', function(accounts) {
   });
 
   it('Main account should be able to transfer balance', async function() {
-    const balanceToTransfer = initBalance / 2;
-    await collateralToken.transfer(accounts[1], balanceToTransfer, { from: accounts[0] });
+    const balanceToTransfer = new BN(1000000);
+    await collateralToken.transfer(accounts[1], balanceToTransfer.toString(), {
+      from: accounts[0]
+    });
     const secondAcctBalance = await collateralToken.balanceOf.call(accounts[1]);
     assert.isTrue(secondAcctBalance.eq(balanceToTransfer), "Transfer didn't register correctly");
   });
